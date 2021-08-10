2c5e484b73c452112648a6acac79ec7a1d4963d1
anyasaha <87838833+anyasaha@users.noreply.github.com>
2021-08-10 18:33:34 +0800

    trending old stuff

--- trending.py
+++ trending.py
@@ -1,6 +1,18 @@
+import numpy as np
+from sklearn import preprocessing, cross_validation, neighbors
 import pandas as pd
-df_xlsx = pd.read_excel("Trending Songs.xlsx")
-print(df_xlsx.head(50))
 
-wb = Workbook("Trending Songs.xlsx")
-wb.save("trending.html")
+df_xlsx = pd.read_csv("tracks.csv")
+df.drop('id', 1, inplace=True)
+
+X = np.array(df.drop(['name'],1))
+y = np.array(df['class'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.4)
+
+clf = neighbors.KNeighborsClassifier()
+clf.fit(X_train, y_train)
+
+
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
